cd0ba469b237add595404aab606a19ce283cd53e
Cherro <kaliszewski.dominik@gmail.com>
2019-01-23 19:21:13 +0100

    Add BaseStatisticsRepository

---
+++ mmo_pd_db_client/Repository/BaseStatistics/BaseStatisticsRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity.ModelConfiguration.Configuration;
+using System.Linq;
+namespace mmo_pd_db_client.Repository.BaseStatistics
+{
+    public class BaseStatisticsRepository : BaseRepository<Entities1,BAZOWE_STATYSTYKI>, IBaseStatisticsRepository
+    {
+        public BAZOWE_STATYSTYKI GetById(int id)
+        {
+            var query = GetAll().FirstOrDefault(x => x.ID == id);
+            return query;
+        }
+    }
+}
---
+++ mmo_pd_db_client/Repository/BaseStatistics/IBaseStatisticsRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace mmo_pd_db_client.Repository.BaseStatistics
+{
+    public interface IBaseStatisticsRepository
+    {
+        BAZOWE_STATYSTYKI GetById(int id);
+    }
+}
