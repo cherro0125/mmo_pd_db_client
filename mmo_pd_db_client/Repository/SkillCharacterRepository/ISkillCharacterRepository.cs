0fbdb8e87384caa54ae7ed7d10f1d305c6ea71eb
Cherro <kaliszewski.dominik@gmail.com>
2019-01-23 20:00:41 +0100

    Add SkillCharacter repository

---
+++ mmo_pd_db_client/Repository/SkillCharacterRepository/ISkillCharacterRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace mmo_pd_db_client.Repository.SkillCharacterRepository
+{
+    public interface ISkillCharacterRepository
+    {
+        UMIEJETNOSCI_POSTAC GetById(int id);
+    }
+}
---
+++ mmo_pd_db_client/Repository/SkillCharacterRepository/SkillCharacterRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+namespace mmo_pd_db_client.Repository.SkillCharacterRepository
+{
+    public class SkillCharacterRepository : BaseRepository<MmoContext,UMIEJETNOSCI_POSTAC>, ISkillCharacterRepository
+    {
+        public UMIEJETNOSCI_POSTAC GetById(int id)
+        {
+            var query = GetAll().FirstOrDefault(x => x.ID == id);
+            return query;
+        }
+    }
+}
