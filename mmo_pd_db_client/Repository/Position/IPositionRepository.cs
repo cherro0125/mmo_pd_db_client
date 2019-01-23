91bb503d1d4aeb90d25e3ea5a909024fe4c6e4c4
Cherro <kaliszewski.dominik@gmail.com>
2019-01-23 19:31:30 +0100

    Add PositionRepository

---
+++ mmo_pd_db_client/Repository/Position/IPositionRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace mmo_pd_db_client.Repository.Position
+{
+    public interface IPositionRepository
+    {
+        POZYCJE GetById(int id);
+    }
+}
---
+++ mmo_pd_db_client/Repository/Position/PositionRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+namespace mmo_pd_db_client.Repository.Position
+{
+    public class PositionRepository : BaseRepository<Entities1,POZYCJE>, IPositionRepository
+    {
+        public POZYCJE GetById(int id)
+        {
+            var query = GetAll().FirstOrDefault(x => x.ID == id);
+            return query;
+        }
+    }
+}
