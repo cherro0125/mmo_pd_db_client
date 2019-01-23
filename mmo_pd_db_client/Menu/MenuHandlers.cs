f8bc3fb56624675a9ceaa2ee53e292760d53b481
Cherro <kaliszewski.dominik@gmail.com>
2019-01-23 21:29:31 +0100

    Add db manual menu handlers

--- mmo_pd_db_client/Menu/MenuHandlers.cs
+++ mmo_pd_db_client/Menu/MenuHandlers.cs
@@ -1,7 +1,74 @@
-﻿namespace mmo_pd_db_client.Menu
+﻿using mmo_pd_db_client.Manual.DB;
+using mmo_pd_db_client.UnitOfWork;
+
+namespace mmo_pd_db_client.Menu
 {
     public class MenuHandlers
     {
-        
+        private IUnitOfWork _unitOfWork;
+        private DbOperation _dbOperation;
+
+        public MenuHandlers()
+        {
+            _unitOfWork = new UnitOfWork.UnitOfWork();       
+            _dbOperation = new DbOperation();            
+        }
+
+        #region ManualDB
+
+        public void CreateTables()
+        {
+            _dbOperation.CreateTables();
+        }
+
+        public void DropTables()
+        {
+            _dbOperation.DropTables();
+        }
+
+        public void CreateSequences()
+        {
+            _dbOperation.CreateSequences();
+        }
+
+        public void DropSequences()
+        {
+            _dbOperation.DropSequences();
+        }
+
+        public void CreatePackages()
+        {
+            _dbOperation.CreatePackages();
+        }
+
+        public void DropPackages()
+        {
+            _dbOperation.DropPackages();
+        }
+
+        public void CreateTriggers()
+        {
+            _dbOperation.CreateTriggers();
+        }
+
+        public void DropTriggers()
+        {
+            _dbOperation.DropTriggers();
+        }
+
+        public void CreateViews()
+        {
+            _dbOperation.CreateViews();
+        }
+
+
+
+        #endregion
+
+        #region ORM
+
+      
+
+        #endregion
     }
 }
