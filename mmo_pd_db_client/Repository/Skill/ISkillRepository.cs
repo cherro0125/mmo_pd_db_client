fa091d3e82fb2a56d75f72dfebc15a4cfb0ccbae
Cherro <kaliszewski.dominik@gmail.com>
2019-01-23 19:57:25 +0100

    Add skill repository

---
+++ mmo_pd_db_client/Repository/Skill/ISkillRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace mmo_pd_db_client.Repository
+{
+    public interface ISkillRepository
+    {
+        UMIEJETNOSCI GetById(int id);
+    }
+}
---
+++ mmo_pd_db_client/Repository/Skill/SkillRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+namespace mmo_pd_db_client.Repository
+{
+    public class SkillRepository : BaseRepository<MmoContext,UMIEJETNOSCI>, ISkillRepository
+    {
+        public UMIEJETNOSCI GetById(int id)
+        {
+            var query = GetAll().FirstOrDefault(x => x.ID == id);
+            return query;
+        }
+    }
+}
