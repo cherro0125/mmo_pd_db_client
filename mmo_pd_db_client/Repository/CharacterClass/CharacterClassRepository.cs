ce1a6023e31e604664261a6f412ad274beda678d
Cherro <kaliszewski.dominik@gmail.com>
2019-01-23 19:27:36 +0100

    Add CharacterClassRepository

---
+++ mmo_pd_db_client/Repository/CharacterClass/CharacterClassRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+namespace mmo_pd_db_client.Repository.CharacterClass
+{
+    public class CharacterClassRepository : BaseRepository<Entities1,KLASY_POSTACI>
+    {
+        public KLASY_POSTACI GetById(int id)
+        {
+            var query = GetAll().FirstOrDefault(x => x.ID == id);
+            return query;
+        }
+    }
+}
---
+++ mmo_pd_db_client/Repository/CharacterClass/ICharacterClassRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace mmo_pd_db_client.Repository.CharacterClass
+{
+    public interface ICharacterClassRepository
+    {
+        KLASY_POSTACI GetById(int id);
+    }
+}
