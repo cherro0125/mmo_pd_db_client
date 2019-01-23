356d0a53528565e440efa314717467b058ac32c8
Cherro <kaliszewski.dominik@gmail.com>
2019-01-23 20:07:47 +0100

    Add CharacterLevelAvg Repo

---
+++ mmo_pd_db_client/Repository/ViewRepository/CharacterLevelAvg/CharacterLevelAvgRepo.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq;
+
+namespace mmo_pd_db_client.Repository.ViewRepository.CharacterLevelAvg
+{
+    public class CharacterLevelAvgRepo : BaseRepository<MmoContext,POSTACIE_LEVEL_AVG>, ICharacterLevelAvgRepo
+    {
+    
+    }
+}
---
+++ mmo_pd_db_client/Repository/ViewRepository/CharacterLevelAvg/ICharacterLevelAvgRepo.cs
@@ -0,0 +1,7 @@
+﻿namespace mmo_pd_db_client.Repository.ViewRepository.CharacterLevelAvg
+{
+    public interface ICharacterLevelAvgRepo
+    {
+        
+    }
+}
