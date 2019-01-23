543cf4c85b5815581d7ce175278c51df4869a013
Cherro <kaliszewski.dominik@gmail.com>
2019-01-23 20:13:03 +0100

    Add CharacterHeightMap view repo

---
+++ mmo_pd_db_client/Repository/ViewRepository/CharacterHeighMap/CharacterHeighMapRepository.cs
@@ -0,0 +1,8 @@
+﻿
+namespace mmo_pd_db_client.Repository.ViewRepository.CharacterHeighMap
+{
+    public class CharacterHeighMapRepository : BaseRepository<MmoContext,POSTACIE_WYSOKOSC_MAPA>, ICharacterHeightMapRepository
+    {
+ 
+    }
+}
