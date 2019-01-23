0fc103f765cfa8841dc05c39b0e7b053f3e7b01f
Cherro <kaliszewski.dominik@gmail.com>
2019-01-23 19:13:41 +0100

    Add LookRepository

---
+++ mmo_pd_db_client/Repository/Look/ILookRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace mmo_pd_db_client.Repository.Look
+{
+    public interface ILookRepository
+    {
+        WYGLAD GetById(int id);
+    }
+}
---
+++ mmo_pd_db_client/Repository/Look/LookRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+namespace mmo_pd_db_client.Repository.Look
+{
+    public class LookRepository : BaseRepository<Entities1,WYGLAD>, ILookRepository
+    {
+        public WYGLAD GetById(int id)
+        {
+            var query = GetAll().FirstOrDefault(x => x.ID == id);
+            return query;
+        }
+    }
+}
