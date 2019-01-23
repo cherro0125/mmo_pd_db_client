5714b70eac9e989b55c20c7e0b067b239d2d0d57
Cherro <kaliszewski.dominik@gmail.com>
2019-01-23 19:23:58 +0100

    Add SkillTypeRepository

---
+++ mmo_pd_db_client/Repository/SkillType/ISkillTypeRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace mmo_pd_db_client.Repository.SkillType
+{
+    public interface ISkillTypeRepository
+    {
+        TYPY_UMIEJETNOSCI GetById(int id);
+    }
+}
---
+++ mmo_pd_db_client/Repository/SkillType/SkillTypeRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+namespace mmo_pd_db_client.Repository.SkillType
+{
+    public class SkillTypeRepository : BaseRepository<Entities1,TYPY_UMIEJETNOSCI>
+    {
+        public TYPY_UMIEJETNOSCI GetById(int id)
+        {
+            var query = GetAll().FirstOrDefault(x => x.ID == id);
+            return query;
+        }
+    }
+}
