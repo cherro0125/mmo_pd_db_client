2917563ae2ae31078874ef85f6602c68e9293831
Cherro <kaliszewski.dominik@gmail.com>
2019-01-23 19:29:03 +0100

    Add RaceRepository

---
+++ mmo_pd_db_client/Repository/Race/IRaceRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace mmo_pd_db_client.Repository.Race
+{
+    public interface IRaceRepository
+    {
+        RASY GetById(int id);
+    }
+}
---
+++ mmo_pd_db_client/Repository/Race/RaceRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+namespace mmo_pd_db_client.Repository.Race
+{
+    public class RaceRepository : BaseRepository<Entities1,RASY>, IRaceRepository
+    {
+        public RASY GetById(int id)
+        {
+            var query = GetAll().FirstOrDefault(x => x.ID == id);
+            return query;
+        }
+    }
+}
