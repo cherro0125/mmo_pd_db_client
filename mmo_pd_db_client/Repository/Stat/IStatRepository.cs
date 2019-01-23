882252d3a4b2934a4052f5a2766f6aa9f21a3ada
Cherro <kaliszewski.dominik@gmail.com>
2019-01-23 19:25:45 +0100

    Add StatRepository

---
+++ mmo_pd_db_client/Repository/Stat/IStatRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace mmo_pd_db_client.Repository.Stat
+{
+    public interface IStatRepository
+    {
+        STATYSTYKI GetById(int id);
+    }
+}
---
+++ mmo_pd_db_client/Repository/Stat/StatRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+namespace mmo_pd_db_client.Repository.Stat
+{
+    public class StatRepository : BaseRepository<Entities1,STATYSTYKI>, IStatRepository
+    {
+        public STATYSTYKI GetById(int id)
+        {
+            var query = GetAll().FirstOrDefault(x => x.ID == id);
+            return query;
+        }
+    }
+}
